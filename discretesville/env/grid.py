46f4ebd78117edde52147d5a22125d2e21551bd5
felipe <felipea2@illinois.edu>
2020-01-24 14:14:05 -0600

    Added simple comments

--- discretesville/env/grid.py
+++ discretesville/env/grid.py
@@ -1,22 +1,44 @@
 from env.vertex import Vertex
 
 class Grid:
+    """ Data structure that stores all of the vertices in a multi-dimensional list.
+        
+    Parameters
+    ----------
+    rows
+        The number of rows in the grid.
+    cols
+        The number of columns in the grid.
+  
+    Attributes
+    ----------
+    rows
+        The number of rows in the grid.
+    cols
+        The number of columns in the grid.
+    vertices
+        A two-dimensional list that stores the vertices.
+    """
+
     def __init__(self, rows, cols):
         self.rows = rows
         self.cols = cols
         self.vertices = [[Vertex(False, (i,j)) for j in range(cols)] for i in range(rows)]
 
-    def debugPrint(self):
-        for row in self.vertices:
-            t = []
-            for v in row:
-                if v.staticObstacle:
-                    t.append(1)
-                else:
-                    t.append(0)
-            print(t)
 
     def getNeighbors(self, vertex):
+        """ Get neighbors of vertex that are not static obstacles.
+    
+        Parameters
+        ----------
+        vertex
+            The vertex whose neighbors should be returned.
+
+        Returns
+        ----------
+        neighbors
+            A list of vertex objects which are neighbors of the requested vertex.
+        """
         row, col = vertex.pos
         neighbors = []
         if row < self.rows - 1 and not self.vertices[row+1][col].isStaticObstacle:
@@ -32,12 +54,21 @@ class Grid:
 
     #Get all unvisited vertices that are not static obstacles
     def getAll(self):
-        out = []
+        """Get all vertices that are not static obstacles.
+    
+  
+        Returns
+        ----------
+        nonObstacles
+            A list of all vertices that are not static obstacles.
+
+        """
+        nonObstacles = []
         for i in range(self.rows):
             for j in range(self.cols):
                 if not self.vertices[i][j].isStaticObstacle:
-                    out.append(self.vertices[i][j])
-        return out
+                    nonObstacles.append(self.vertices[i][j])
+        return nonObstacles
     
     def getVertex(self, x, y):
         return self.vertices[x][y]
